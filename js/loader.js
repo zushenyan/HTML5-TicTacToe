7b9df109f2ca25194e8b64e89f38e0d2a6f9e46a
Andrew Yan <zushenyan@gmail.com>
2015-09-03 18:10:41 +0800

    simplified loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,17 +1,5 @@
 requirejs(
 	[
-		"ttt/TicTacToe",
-		"ttt/Resources",
-		"ttt/GameObject",
-		"ttt/Chess",
-		"ttt/Animation",
-		"ttt/AnimationFactory",
-		"ttt/Board",
-		"ttt/BoardManager",
 		"ttt/Main",
-		// "ttt/test"
-	],
-	function(){
-
-	}
+	]
 );
